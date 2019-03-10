2a6c700ff05a9094fa48de86201f8a4babe9651b
spx0315 <1445024781@qq.com>
2019-03-10 11:21:42 +0800

    test

--- src/main/java/com/spx0315/dao/UserDao.java
+++ src/main/java/com/spx0315/dao/UserDao.java
@@ -1,9 +1,11 @@
 package com.spx0315.dao;
 
 import com.spx0315.pojo.User;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface UserDao {
     int addUser(User user);
 
